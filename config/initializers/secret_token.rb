73827810a97ca2054bf9c602c0691d657aac4eef
Itagaki Kohta <ikaya3@gmail.com>
2013-12-03 16:19:54 +0900

    rails new した。

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tagatter::Application.config.secret_token = '0e07225d11c912b9fed959b6db228a655d47e0f1731bca51649c10bb3e42d672a1c45a269548c6a472825ebc42da9e329f14ff89d70e71603737adb646edaa4d'
